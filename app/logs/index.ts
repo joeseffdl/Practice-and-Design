c7fc52d8336b4edfca523cafd535f96e0123b616
Joseph De Leon <joeseffdl@gmail.com>
2023-05-06 22:20:44 +0800

    Created weather header component

--- app/logs/index.ts
+++ app/logs/index.ts
@@ -1,5 +1,7 @@
 import ListItem from "./ListItem";
 import MyHome from "./MyHome";
 import WeatherGridItem from "./WeatherGridItem";
+import WeatherHeader from "./WeatherHeader";
 
-export { ListItem, WeatherGridItem, MyHome }
+
+export { ListItem, WeatherGridItem, MyHome, WeatherHeader }
